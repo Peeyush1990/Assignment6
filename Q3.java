38b77dca1b7f3245942b498cc87bcf5c37740706
Peeyush1990 <112894791+Peeyush1990@users.noreply.github.com>
2022-12-02 12:12:58 +0530

    Update Q3.java

--- Q3.java
+++ Q3.java
@@ -1,85 +1,76 @@
-import java.util.*;
-class calculator{
-    int a,b;
-    calculator(int a,int b){
-        this.a=a;
-        this.b=b;
-    }
-}  
+import java.util.Scanner;
+
+class Calculator {
+    private int num1; 
+    private int num2;
+
+     public Calculator(int num1, int num2) { 
+         this.num1 = num1;
+         this.num2 = num2;
+     }
+
+ 	public int addition() {  
+ 		int ans = num1 + num2; 
+		System.out.println("Sum : " + ans);
+		return ans;
+	}
+	
+	public int subtraction() { 
+		int ans = num1 - num2;
+		System.out.println("Difference: " + ans);
+		return ans;
+	}
+	
+	public double division() {
+		double ans = (double) num1 / num2; 
+		System.out.println("Division : " +ans);
+		return ans;
+	}
+	
+	public int multiply() {
+		int ans = num1 * num2; 
+	    System.out.println("Product : " + ans);
+	    return ans;
+	}
+ }
+
 public class Q3 {
-    static void Add(calculator obj){ 
-        int sum=obj.a+obj.b;
-        System.out.println("The sum is: "+sum);
+	 public static void main(String ar[]) {
+
+		 Scanner scn = new Scanner(System.in);
+		 int num1, num2;
+		 try {
+			 num1 = Integer.parseInt(scn.nextLine());
+			 num2 = Integer.parseInt(scn.nextLine());
+			 
+			 Calculator obj1 = new Calculator(num1, num2); 
+			 obj1.addition(); 
+			 
+			
+			 num1 = Integer.parseInt(scn.nextLine());
+			 
+			 num2 = Integer.parseInt(scn.nextLine());
+			 
+			 Calculator obj2 = new Calculator(num1, num2); 
+			 obj2.subtraction(); 
+			 
+			 
+			 num1 = Integer.parseInt(scn.nextLine());
+			 num2 = Integer.parseInt(scn.nextLine());
+			 
+			 Calculator obj3 = new Calculator(num1, num2);
+			 obj3.multiply(); 
+			 
+			 
+			 num1 = Integer.parseInt(scn.nextLine());
+			 num2 = Integer.parseInt(scn.nextLine());
+			 
+			 Calculator obj4 = new Calculator(num1, num2); 
+			 obj4.division(); 
+		 }
+		 catch(NumberFormatException e) { 
+			 System.out.println("invalid input " + e);
+		 }
+         scn.close();
+	 }
     }
-    static void Subtract(calculator obj){
-        int difference=obj.a-obj.b;
-        System.out.println("The difference is: "+difference);
-    }
-    static void Multiply(calculator obj){
-        int product=obj.a*obj.b;
-        System.out.println("The product is: "+product);
-    }
-    static void Division(calculator obj) {
-        int quotient=obj.a/obj.b;
-        System.out.println("The quoteint after division is: "+quotient);
-   }
-
-   public static int inputNum(){
-
-    try{
-        Scanner sc=new Scanner(System.in);
-        int num=0;
-        num=sc.nextInt();//for addition
-        return num;
-       }catch(InputMismatchException e){
-        System.out.println("Please enter integer only");
-       }
-       return 0;
-   }
-    public static void main(String args[]){
-        
-        Scanner sc=new Scanner(System.in);
-        int a,b;
-    //    try{ 
-    //     System.out.print("Please enter numbers: ");
-    //     a=sc.nextInt(); b=sc.nextInt(); //for addition 
-        // calculator obj1=new calculator(a,b);
-        // Add(obj1);
-    //    }catch(InputMismatchException e){
-    //     System.out.println("Please enter integer only");
-    //    }
-        a = inputNum();
-        b = inputNum();
-        calculator obj1=new calculator(.,b);
-        Add(obj1);
-
-        try{
-        System.out.print("Please enter numbers: ");
-        a=sc.nextInt(); b=sc.nextInt(); //for subtraction
-        calculator obj2=new calculator(a,b);
-        Subtract(obj2);
-        }catch(InputMismatchException e){
-            System.out.println("Please enter integer only");
-           }
-           finally{}
-
-        try{
-        System.out.print("Please enter numbers: ");
-        a=sc.nextInt(); b=sc.nextInt(); //for multiplication
-        calculator obj3=new calculator(a,b);
-        Multiply(obj3);
-        }catch(InputMismatchException e){
-            System.out.println("Please enter integer only");
-        }
-
-        try{
-        System.out.print("Please enter numbers: ");
-        a=sc.nextInt(); b=sc.nextInt(); //for division
-        calculator obj4=new calculator(a,b);
-        Division(obj4);
-        }catch(InputMismatchException e){
-            System.out.println("Please enter integer only");
-           }
-        sc.close();  
-    }
-}
